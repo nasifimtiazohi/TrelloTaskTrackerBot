53519d033a5df6ff96f1f948efe6cf306f32cda8
Otto <yuh198@ucsd.edu>
2017-11-01 21:50:13 -0400

    add envTest for Slack_BOT

---
+++ envTest.py
@@ -0,0 +1,10 @@
+import os
+from slackclient import SlackClient
+
+# BOT_NAME = 'taskbot'
+# slack_client = SlackClient(os.environ.get('BOT_TOKEN'))
+if not ((os.environ.get('BOT_TOKEN') is None) and (os.environ.get('BOT_ID') is None)):
+    print(os.environ.get('BOT_TOKEN'))
+    print(os.environ.get('BOT_ID'))
+else:
+    print('You did not setup the Slack_BOT_Token. ')
