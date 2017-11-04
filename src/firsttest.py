da1c536334f6bf298734743e65d48bbe265c621a
simtiaz <simtiaz@ncsu.edu>
2017-11-04 14:52:47 -0400

    saturday commit

--- src/firsttest.py
+++ src/firsttest.py
@@ -27,16 +27,6 @@ def handle_command(command, channel):
         response = "Sure...write some more code then I can do that!"
         slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
-    # elif COMMAND_USECASE_1 in command:
-    #     messages=trellocall.print_deadline_messages()
-    #     for m in messages:
-    #         slack_client.api_call("chat.postMessage", channel=channel,
-    #                       text=m, as_user=True)
-    #     #TODO: check it on timely basis
-    #         ''' will be done in milestone 3 '''
-    #     #TODO: send mail to the user
-    #         ''' retrieve mail_id of users from mock.json
-    #         send an email '''
     elif command.startswith(COMMAND_USECASE_2):
         messages=trellocall.print_members_points()
         for key in messages.keys():
@@ -45,17 +35,15 @@ def handle_command(command, channel):
                           text=message, as_user=True)
         #todo
     elif COMMAND_USECASE_3 in command:
-        #todo
-        ''' retrieve cards that are due : done
-        detect direct message channel for each user: done
-        ask for progress: done
-        capture response: required
-        open a new public channel named "reminderBuddy": required
-        post reminders in the channel: required '''
         response="what is your progress, mate?"
         dm_channels=usecase3.perform()
         for d in dm_channels:
             channel=d[2]
+            response=d[4]
+            # cardlist=d[3]
+            # print "bal",channel
+            # for c in cardlist:
+            #     response+=c.name
             slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
 
@@ -73,6 +61,8 @@ def parse_slack_output(slack_rtm_output):
     output_list = slack_rtm_output
     if output_list and len(output_list) > 0:
         for output in output_list:
+            if 'text' in output:
+                print output['text']
             if output and 'text' in output and AT_BOT in output['text']:
                 # return text after the @ mention, whitespace removed
                 #todo: only works with texts after the mention, need to fix
@@ -82,9 +72,9 @@ def parse_slack_output(slack_rtm_output):
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
-        print("StarterBot connected and running!")
+        print("Taskbot connected and running!")
         while True:
-            usecase1.perform()
+            #usecase1.perform()
             command, channel = parse_slack_output(slack_client.rtm_read())
             if command and channel:
                 handle_command(command, channel)
